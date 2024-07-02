36482ace2720bde14237ddfb2a6a067d77217b39
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2024-07-02 11:38:07 +0200

    Update handler.rs

--- src/api/handler.rs
+++ src/api/handler.rs
@@ -6,7 +6,7 @@ use chrono::prelude::*;
 use futures::StreamExt;
 use serde::{Deserialize, Serialize};
 use std::env;
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, path::Path};
 use uuid::Uuid;
 
 use crate::server::message::BroadcastMessage;
@@ -30,6 +30,7 @@ pub struct GenericResponse<T> {
 pub enum MessageType {
     Text,
     Image,
+    Video,
 }
 
 // Endpoint to send message
@@ -86,6 +87,22 @@ async fn upload_image(
                     return Ok(HttpResponse::BadRequest().json(_response_json));
                 }
 
+                // Check if the file has .jpg, .png, or .webp extension
+                let file_extension = Path::new(filename)
+                    .extension()
+                    .and_then(std::ffi::OsStr::to_str);
+                if file_extension != Some("jpg")
+                    && file_extension != Some("png")
+                    && file_extension != Some("webp")
+                {
+                    let response_json = GenericResponse {
+                        status: "Unsupported media type".to_string(),
+                        message: "Only .jpg, .png, or .webp files are allowed.".to_string(),
+                        value: vec![{}].into(),
+                    };
+                    return Ok(HttpResponse::UnsupportedMediaType().json(response_json));
+                }
+
                 // Add id with the file name original
                 let filename_with_id = format!("{}_{}", id, filename);
 
@@ -106,7 +123,7 @@ async fn upload_image(
         }
     }
     // Send the file path to WebSocket clients
-    let file_path_str = file_path.to_string();
+    let file_path_str = format!("/{}", file_path.to_string());
 
     let broadcast_message = BroadcastMessage {
         message: file_path_str.to_string(),
@@ -124,6 +141,91 @@ async fn upload_image(
     Ok(HttpResponse::Created().json(response_json))
 }
 
+async fn upload_video(
+    data: web::Data<Addr<AppState>>,
+    mut payload: Multipart,
+) -> Result<HttpResponse, Error> {
+    // Get the upload folder
+    let upload_dir = env::current_dir().unwrap().join("uploads/video");
+
+    // Create the upload folder if doesnt exist
+    if !upload_dir.exists() {
+        std::fs::create_dir(&upload_dir).unwrap();
+    }
+
+    // Generate an Uuid before each loop
+    let id = Uuid::new_v4();
+
+    // Declare the variable before entering the loop
+    let mut file_path = String::new();
+
+    while let Some(item) = payload.next().await {
+        match item {
+            Ok(mut field) => {
+                let content_disposition = field.content_disposition();
+                let filename = content_disposition.get_filename().unwrap_or("unknown");
+
+                //Check if file is uploaded
+                if filename.len() == 0 {
+                    let _response_json = GenericResponse {
+                        status: "Bad request".to_string(),
+                        message: "No file found...".to_string(),
+                        value: vec![{}].into(),
+                    };
+                    return Ok(HttpResponse::BadRequest().json(_response_json));
+                }
+
+                // Check if the file has .mp4 extension
+                let file_extension = Path::new(filename)
+                    .extension()
+                    .and_then(std::ffi::OsStr::to_str);
+                if file_extension != Some("mp4") {
+                    let response_json = GenericResponse {
+                        status: "Unsupported media type".to_string(),
+                        message: "Only .mp4 files are allowed.".to_string(),
+                        value: vec![{}].into(),
+                    };
+                    return Ok(HttpResponse::UnsupportedMediaType().json(response_json));
+                }
+
+                // Add id with the file name original
+                let filename_with_id = format!("{}_{}", id, filename);
+
+                file_path = format!("uploads/video/{}", filename_with_id);
+                //Upload the file into folder uploads/
+                let mut file = File::create(file_path.clone()).unwrap();
+
+                // Copy the content of the field to the file
+                while let Some(chunk) = field.next().await {
+                    let data = chunk.unwrap();
+                    file.write_all(&data).unwrap();
+                }
+            }
+            //If a error return error 500
+            Err(e) => {
+                return Ok(HttpResponse::InternalServerError().body(format!("Error: {:?}", e)));
+            }
+        }
+    }
+    // Send the file path to WebSocket clients
+    let file_path_str = format!("/{}", file_path.to_string());
+
+    let broadcast_message = BroadcastMessage {
+        message: file_path_str.to_string(),
+        type_message: MessageType::Video,
+        created_at: Utc::now(),
+    };
+    data.do_send(broadcast_message.clone());
+
+    let response_json = GenericResponse {
+        status: "created".to_string(),
+        message: "Video uploaded and broadcasted".to_string(),
+        value: vec![broadcast_message].into(),
+    };
+
+    Ok(HttpResponse::Created().json(response_json))
+}
+
 // fallback route
 async fn handler_404() -> HttpResponse {
     HttpResponse::NotFound().body("404 : Nothing here..")
@@ -133,6 +235,8 @@ async fn handler_404() -> HttpResponse {
 pub fn config(conf: &mut web::ServiceConfig) {
     conf.route("/send-message", web::post().to(send_message))
         .route("/upload-image", web::post().to(upload_image))
+        .route("/upload-video", web::post().to(upload_video))
         .service(Files::new("/uploads/img", "uploads/img").show_files_listing())
+        .service(Files::new("/uploads/video", "uploads/video").show_files_listing())
         .default_service(web::to(handler_404));
 }
